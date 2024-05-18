1689efd398217712bc07358a0c11a254283d18fe
Your Name <you@example.com>
2024-05-18 09:47:03 -0400

    Page6

--- server/djangoproj/urls.py
+++ server/djangoproj/urls.py
@@ -18,13 +18,11 @@ from django.urls import path, include
 from django.views.generic import TemplateView
 from django.conf.urls.static import static
 from django.conf import settings
-from djangoapp import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('djangoapp/', include('djangoapp.urls')),
         path('about/', TemplateView.as_view(template_name="About.html")),
             path('contact/', TemplateView.as_view(template_name="Contact.html")),
-            path(route='login', view=views.login_user, name='login'),
                 path('login/', TemplateView.as_view(template_name="index.html")),
     path('', TemplateView.as_view(template_name="Home.html")),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
