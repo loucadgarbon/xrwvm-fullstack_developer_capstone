48a0c46ec425464853246da835d457923debaf31
Your Name <you@example.com>
2024-05-19 05:44:57 -0400

    Week5 Lab2 done

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-ccow$tz_=9%dxu4(0%^(z%nx32#s@(zt9$ih@)5l54yny) \
+SECRET_KEY = "django-insecure-ccow$tz_=9%dxu4(0%^(z%nx32#s@(zt9$ih@)5l54yny)\
 wm-0"
 
 # SECURITY WARNING: don't run with debug turned on in production!
@@ -28,11 +28,11 @@ DEBUG = True
 
 ALLOWED_HOSTS = [
     "localhost",
-    "https://loucadgarbon-8000.theiadockernext-0-labs-prod-theiak8s-4-tor01 \
+    "https://loucadgarbon-8000.theiadockernext-0-labs-prod-theiak8s-4-tor01\
     .proxy.cognitiveclass.ai",
 ]
 CSRF_TRUSTED_ORIGINS = [
-    "https://loucadgarbon-8000.theiadockernext-0-labs-prod-theiak8s-4-tor01 \
+    "https://loucadgarbon-8000.theiadockernext-0-labs-prod-theiak8s-4-tor01\
     .proxy.cognitiveclass.ai"
 ]
 
@@ -100,8 +100,7 @@ DATABASES = {
 AUTH_PASSWORD_VALIDATORS = [
     {
         "NAME":
-        "django.contrib.auth.password_validation. \
-        UserAttributeSimilarityValidator",
+        "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
     },
     {
         "NAME":
